aac78f1df462edb9125814a04f0962d782e78d12
Mamura <marcio.mota@gmail.com>
2025-05-03 11:29:12 -0300

    change config/database to azure

--- src/config/database.php
+++ src/config/database.php
@@ -55,8 +55,8 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            //'options' => extension_loaded('pdo_mysql') ? array_filter([
-            //    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
