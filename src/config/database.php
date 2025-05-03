a28e7d5b4fe11c23797841cf54f3d202d37ab57e
Mamura <marcio.mota@gmail.com>
2025-05-03 11:27:13 -0300

    change config/database to azure

--- src/config/database.php
+++ src/config/database.php
@@ -44,21 +44,19 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', env('AZURE_MYSQL_HOST', '127.0.0.1')),
+            'port' => env('DB_PORT', env('AZURE_MYSQL_PORT', '3306')),
+            'database' => env('DB_DATABASE', env('AZURE_MYSQL_DBNAME', 'forge')),
+            'username' => env('DB_USERNAME', env('AZURE_MYSQL_USERNAME', 'forge')),
+            'password' => env('DB_PASSWORD', env('AZURE_MYSQL_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            //'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
